7d8d7667d66d13f46c2221ba07bc6241ed221d69
Mikle Ugera <ugeramikle@gmail.com>
2026-09-16 19:55:44 +0300

    create header component

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import { About } from './pages/About'
 import { Profile } from './pages/Profile'
 import { SingUp } from './pages/SingUp'
 import { SingIn } from './pages/SingIn'
+import { Header } from './components/Header'
 
 export const App = () => {
   return (
     <BrowserRouter>
+      <Header/>
       <Routes>  
         <Route path='/' element={<Home/>} />
         <Route path='/sing-in' element={<SingIn/>} />
