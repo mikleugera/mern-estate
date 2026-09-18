cd89a2b71fa8d3f98cbc51a0681056fc38d56049
Mikle Ugera <ugeramikle@gmail.com>
2026-09-18 19:34:51 +0300

    create a middleware

--- api/index.js
+++ api/index.js
@@ -24,3 +24,14 @@ app.listen(3000, () => {
 
 app.use('/', UserRouter)
 app.use('/', AuthRouter)
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    })
+})
